3ea9a02f91bf9c3b5f37182ef5e72d171b06d20c
ALIZA STONE <amstone326@gmail.com>
2016-05-23 17:27:10 -0400

    refactor

--- features/step_definitions/widget_entry_steps.rb
+++ features/step_definitions/widget_entry_steps.rb
@@ -27,42 +27,12 @@ end
 #COMPACT SINGLE-SELECT WIDGET
 #TODO: implement actually answering this (right now it just checks that the structure is correct)
 Then (/^I see a compact single-select widget with row labels "([^\"]*)" and options "([^\"]*)"$/) do |row_labels, options|
+  
   row_labels = row_labels.split(",")
+  test_text_views_present_in_order(row_labels)
+
   options = options.split(",")
-
-  all_text_views = query("android.widget.TextView")
-
-  # Assert that all of the row labels are present, and appear in the order they were inputted
-  index_of_previous_label = -1
-  previous_label = ""
-  for row_label in row_labels
-    if element_exists("android.widget.TextView text:'#{row_label}'")
-      index_in_dom = all_text_views.index(query("android.widget.TextView text:'#{row_label}'").first)
-      if index_in_dom < index_of_previous_label
-        fail("Row labels are not present in the order provided. %s appears before %s" % [row_label, previous_label])
-      index_of_previous_label = index_in_dom
-      previous_label = row_label
-      end
-    else
-      fail("Missing expected row label %s" % [row_label])
-    end
-  end
-
-  # Assert that all of the options are present, and appear in the order they were inputted
-  index_of_previous_option = -1
-  previous_option = ""
-  for option in options
-    if element_exists("android.widget.TextView text:'#{option}'")
-      index_in_dom = all_text_views.index(query("android.widget.TextView text:'#{option}'").first)
-      if index_in_dom < index_of_previous_option
-        fail("Options are not present in the order provided. %s appears before %s" % [option, previous_option])
-      index_of_previous_option = index_in_dom
-      previous_option = option
-      end
-    else
-      fail("Missing expected option %s" % [option])
-    end
-  end
+  test_text_views_present_in_order(options)
 
   # Assert that the number of radio buttons present is equivalent to 1 for each option, for each row label
   num_possible_choices = row_labels.length * options.length
@@ -72,6 +42,27 @@ Then (/^I see a compact single-select widget with row labels "([^\"]*)" and opti
   end
 end
     
+
+# Assert that all of the given text views are present, and appear in the order they were inputted
+def test_text_views_present_in_order(labels)
+  all_text_views = query("android.widget.TextView")
+
+  index_of_previous_label = -1
+  previous_label = ""
+  for label in labels
+    if element_exists("android.widget.TextView text:'#{label}'")
+      index_in_dom = all_text_views.index(query("android.widget.TextView text:'#{label}'").first)
+      if index_in_dom < index_of_previous_label
+        fail("Text views are not present in the order provided. %s appears before %s" % [label, previous_label])
+      index_of_previous_label = index_in_dom
+      previous_label = label
+      end
+    else
+      fail("Missing expected text view %s" % [label])
+    end
+  end
+end
+
 
 # MULTI SELECT WIDGET
 Then (/^I select option from single-select with text "([^\"]*)"$/) do |target_text|
