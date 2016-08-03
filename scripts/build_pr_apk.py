b41eb552a2a6f877bc5238f697f979f334753982
Phillip Mates <pmates@dimagi.com>
2016-08-03 11:37:58 -0400

    Update test runner script

--- scripts/build_pr_apk.py
+++ scripts/build_pr_apk.py
@@ -6,14 +6,14 @@ import subprocess
 import sys
 from checkout_cross_request_repo import checkout_pr_branch
 
-APK_FILE = "build/outputs/apk/commcare-odk-commcare-debug.apk"
-ODK_DIR = "commcare-odk"
+APK_FILE = "build/outputs/apk/commcare-android-commcare-debug.apk"
+ODK_DIR = "commcare-android"
 
 
 def run(running_dir, pr_number):
     parent_dir = os.path.abspath("../")
-    checkout_pr_branch(parent_dir, pr_number, "commcare-odk", "commcare")
-    checkout_pr_branch(parent_dir, pr_number, "commcare-odk", "javarosa")
+    checkout_pr_branch(parent_dir, pr_number,
+                       "commcare-android", "commcare-core")
     os.chdir(os.path.join(parent_dir, ODK_DIR))
     os.remove(APK_FILE)
     subprocess.call(["gradle", "assembleCommcareDebug"])
@@ -30,8 +30,7 @@ def main():
         sys.exit()
 
     # assumes that this script is run from commcare-ui-tests base dir and
-    # odk/javarosa/commcare dirs are all share the same parent dir, one level
-    # up
+    # android/core dirs are all share the same parent dir, one level up
     pr_number = sys.argv[1]
     run(os.getcwd(), pr_number)
 
--- scripts/get_latest_apk_and_resign.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget "https://jenkins.dimagi.com/job/commcare-android/lastSuccessfulBuild/artifact/commcare-android/build/outputs/apk/commcare-android-commcare-release.apk" -O commcare.apk
-calabash-android resign commcare.apk
