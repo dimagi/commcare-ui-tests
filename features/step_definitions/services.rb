93fe0a650543d660ae5d7f1059e3f298e66ab2ef
Phillip Mates <pmates@dimagi.com>
2016-08-19 14:51:01 -0400

    Cleanup airplane mode toggle

--- features/step_definitions/services.rb
+++ features/step_definitions/services.rb
@@ -1,5 +1,5 @@
 Then (/^I toggle airplaine mode$/) do
-  system("adb shell am start -a android.settings.AIRPLANE_MODE_SETTINGS")
-  system("adb shell input keyevent 19 & adb shell input keyevent 23")
-  press_back_button
+  system("adb shell am start -a android.settings.AIRPLANE_MODE_SETTINGS && sleep 0.3")
+  system("adb shell input keyevent 19 && sleep 0.3 && adb shell input keyevent 23")
+  system("adb shell input keyevent 4")
 end
