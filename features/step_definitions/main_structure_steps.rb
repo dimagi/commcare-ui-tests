1b08193d86f8b420606111a20ced0c0be22492be
nsclark <nclark@dimagi.com>
2016-10-13 15:57:37 -0400

    Update main_structure_steps.rb

--- features/step_definitions/main_structure_steps.rb
+++ features/step_definitions/main_structure_steps.rb
@@ -31,8 +31,6 @@ end
 Then (/^I select case "([^\"]*)"$/) do |text|
    wait_for_element_exists("* id:'screen_entity_select_list'")
    tap_when_element_exists("* {text CONTAINS[c] '#{text}'}")
-   # wait for form loading to finish
-  wait_for_element_exists("* id:'nav_pane'")
 end
 
 Then (/^I sync$/) do
