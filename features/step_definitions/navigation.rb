4148e9550a104280a1c840ed8317e8afd706e889
Will Pride <wpride@dimagi.com>
2016-12-14 14:38:42 -0500

    bump time

--- features/step_definitions/navigation.rb
+++ features/step_definitions/navigation.rb
@@ -60,7 +60,7 @@ Then (/^I go back to the home screen$/) do
     # wait before checking if we are on the home screen because activity
     # dispatch goes through the home screen causing this to always be true if
     # checked immediately
-    sleep 1
+    sleep 3
   end
 end
 
