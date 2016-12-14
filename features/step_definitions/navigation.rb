d797a01f9575097b7c9509cacb5b418e6a8c39aa
Will Pride <wpride@dimagi.com>
2016-12-14 14:30:54 -0500

    fix sleep

--- features/step_definitions/navigation.rb
+++ features/step_definitions/navigation.rb
@@ -60,7 +60,7 @@ Then (/^I go back to the home screen$/) do
     # wait before checking if we are on the home screen because activity
     # dispatch goes through the home screen causing this to always be true if
     # checked immediately
-    sleep 1000
+    sleep 1
   end
 end
 
