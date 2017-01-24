1d99a4da918e0a230965f1f6b9de6ddf0f5f5725
ALIZA STONE <amstone326@gmail.com>
2017-01-24 14:27:55 -0500

    attempt to fix update test

--- features/step_definitions/services.rb
+++ features/step_definitions/services.rb
@@ -1,3 +1,5 @@
+# Useful page with all of the keyevent codes and what they do https://groups.google.com/forum/#!topic/android-developers/pHBcP2JBT5A
+
 Then (/^I toggle airplane mode$/) do
   system("adb shell am start -a android.settings.AIRPLANE_MODE_SETTINGS && sleep 0.5")
   system("adb shell input keyevent 19 && sleep 0.3 && adb shell input keyevent 23")
