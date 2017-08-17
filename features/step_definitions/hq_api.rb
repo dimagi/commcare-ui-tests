d2d3734f83afdd47b2f18da6792f296020e5e1a3
Shubham Goyal <shubham1g5@gmail.com>
2017-08-17 13:54:59 +0530

    Increases timeout for server response to 25 sec

--- features/step_definitions/hq_api.rb
+++ features/step_definitions/hq_api.rb
@@ -75,7 +75,7 @@ end
 
 Then (/^I check that an async restore occurred successfully$/) do
   # Indicates that at least 1 retry response was received
-  wait_for_element_exists("* {text CONTAINS[c] 'Waiting for Server Progress'}'", timeout: 15)
+  wait_for_element_exists("* {text CONTAINS[c] 'Waiting for Server Progress'}'", timeout: 25)
 
   # Allow the async restore to take up to 5 minutes to send back a full response
   wait_for_element_exists("* {text CONTAINS[c] 'Processing Data from Server'}'", timeout: 300)
@@ -85,7 +85,7 @@ end
 
 Then (/^I check that an async incremental sync occurred successfully$/) do
   # Indicates that at least 1 retry response was received
-  wait_for_element_exists("* {text CONTAINS[c] 'Waiting for Server Progress'}'", timeout: 15)
+  wait_for_element_exists("* {text CONTAINS[c] 'Waiting for Server Progress'}'", timeout: 25)
 
   # Allow the async restore to take up to 5 minutes to send back a full response
   wait_for_element_exists("* {text CONTAINS[c] 'Processing Data from Server'}'", timeout: 300)
