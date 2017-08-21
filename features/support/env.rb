e1f305a9738126628db1ba86444fca210a6a4d17
nsclark <nclark@dimagi.com>
2017-08-21 18:07:24 -0400

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,10 @@
 require 'calabash-android/cucumber'
 require 'cucumber_characteristics/autoload'
+
+CucumberCharacteristics.configure do |config|
+	current_time = Time.now.strftime('&Y-%m-%d_%H-%M-%S')
+	config.export_json = true
+	config.export_html = true
+	config.precision = 4
+	config.target_filename = 'cucumber_step_characteristics' << current_time
+end 
