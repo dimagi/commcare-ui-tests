0ca47b1fe2aef41076e32acb1991af37c2ab196e
nsclark <nclark@dimagi.com>
2017-08-21 18:37:16 -0400

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,5 +6,6 @@ CucumberCharacteristics.configure do |config|
 	config.export_json = true
 	config.export_html = true
 	config.precision = 4
-	config.target_filename = 'cucumber_step_characteristics' << current_time
+	config.target_filename = 'metrics' << current_time
+	config.relative_path = 'features/characteristics'
 end 
