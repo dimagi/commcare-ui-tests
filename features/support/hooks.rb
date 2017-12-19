ec195b0cf3c32451f25fa3296ab442af04b29a34
Will Pride <wpride@dimagi.com>
2017-12-19 17:11:37 -0500

    Add permissions hook

---
+++ features/support/hooks.rb
@@ -0,0 +1,14 @@
+require 'calabash-android/management/adb'
+require 'calabash-android/operations'
+
+permission_array = ["CAMERA", "CALL_PHONE", "READ_EXTERNAL_STORAGE", "RECORD_AUDIO", "ACCESS_FINE_LOCATION"]
+
+Before do |scenario|
+  permission_array.each { |permission| grant_permission(permission) }
+end
+
+def grant_permission(permission)
+  puts "Granting permission #{permission}"
+  cmd = "adb shell pm grant org.commcare.dalvik.debug android.permission.#{permission}"
+  raise "Could not set permission for #{permission}" unless system(cmd)
+end
