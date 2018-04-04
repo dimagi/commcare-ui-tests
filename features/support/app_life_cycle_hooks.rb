6da29ed100b4a8f8a84b7af26316ed319bc92ff6
Will Pride <wpride@dimagi.com>
2018-04-04 10:03:41 -0400

    Try casting env to sttring

--- features/support/app_life_cycle_hooks.rb
+++ features/support/app_life_cycle_hooks.rb
@@ -2,7 +2,7 @@ require 'calabash-android/management/adb'
 require 'calabash-android/operations'
 
 Before do |scenario|
-  ENV["RESET_BETWEEN_SCENARIOS"] = 0
+  ENV["RESET_BETWEEN_SCENARIOS"] = '0'
   start_test_server_in_background
 end
 
@@ -10,6 +10,6 @@ After do |scenario|
   if scenario.failed?
     screenshot_embed
   end
-  ENV["RESET_BETWEEN_SCENARIOS"] = 1
+  ENV["RESET_BETWEEN_SCENARIOS"] = '1'
   shutdown_test_server
 end
