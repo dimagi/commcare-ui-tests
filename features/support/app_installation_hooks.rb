92b0279e251c42e01e3921ea61f538145e347d59
Will Pride <wpride@dimagi.com>
2018-04-04 12:58:46 -0400

    try fixing again

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -7,8 +7,8 @@ end
 $first_run = true
 
 Before do |scenario|
+  ENV['RESET_BETWEEN_SCENARIOS'] == '0'
   scenario = scenario.scenario_outline if scenario.respond_to?(:scenario_outline)
-
   feature = scenario.feature
   if FeatureMemory.feature != feature || ENV['RESET_BETWEEN_SCENARIOS'] == '1'
     if ENV['RESET_BETWEEN_SCENARIOS'] == '1'
@@ -17,18 +17,16 @@ Before do |scenario|
       log 'First scenario in feature - reinstalling apps'
     end
 
-    if $first_run
-      $first_run = false
-      uninstall_apps
-      install_app(ENV["TEST_APP_PATH"])
-      install_app(ENV["APP_PATH"])
-    end
-
     FeatureMemory.feature = feature
     FeatureMemory.invocation = 1
   else
     FeatureMemory.invocation += 1
   end
 end
+
+After do |scenario|
+  ENV["RESET_BETWEEN_SCENARIOS"] = '1'
+end
+
 
 FeatureMemory = Struct.new(:feature, :invocation).new
