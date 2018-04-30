1535d105971310462fb2acaefe59a1ac5f6e71d7
Will Pride <wpride@dimagi.com>
2018-04-30 16:28:02 -0400

    Revert "use ensure_app_installed"
    This reverts commit a3613231ef841f01f1dc315fe029e962d9cb5642.

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -9,10 +9,18 @@ Before do |scenario|
 
   feature = scenario.feature
   if FeatureMemory.feature != feature
-    log "Feature memory feature ${FeatureMemory.feature}, scenario feature ${scenario.feature}"
+    if ENV['RESET_BETWEEN_SCENARIOS'] == '1'
+      log 'New scenario - reinstalling apps'
+    else
+      log 'First scenario in feature - reinstalling apps'
+    end
 
-    ensure_app_installed
-
+    if not application_installed?(ENV['TEST_APP_PATH'])
+      install_app(ENV['TEST_APP_PATH'])
+    end
+    if not application_installed?(ENV['APP_PATH'])
+      install_app(ENV['APP_PATH'])
+    end
     FeatureMemory.feature = feature
     FeatureMemory.invocation = 1
   else
