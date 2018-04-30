a3613231ef841f01f1dc315fe029e962d9cb5642
Will Pride <wpride@dimagi.com>
2018-04-30 15:45:28 -0400

    use ensure_app_installed

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -9,18 +9,10 @@ Before do |scenario|
 
   feature = scenario.feature
   if FeatureMemory.feature != feature
-    if ENV['RESET_BETWEEN_SCENARIOS'] == '1'
-      log 'New scenario - reinstalling apps'
-    else
-      log 'First scenario in feature - reinstalling apps'
-    end
+    log "Feature memory feature ${FeatureMemory.feature}, scenario feature ${scenario.feature}"
 
-    if not application_installed?(ENV['TEST_APP_PATH'])
-      install_app(ENV['TEST_APP_PATH'])
-    end
-    if not application_installed?(ENV['APP_PATH'])
-      install_app(ENV['APP_PATH'])
-    end
+    ensure_app_installed
+
     FeatureMemory.feature = feature
     FeatureMemory.invocation = 1
   else
