83b5d42adfff7d1f505934f4d2a956530a94a40e
Will Pride <wpride@dimagi.com>
2018-04-30 16:28:33 -0400

    Add test log

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -9,11 +9,7 @@ Before do |scenario|
 
   feature = scenario.feature
   if FeatureMemory.feature != feature
-    if ENV['RESET_BETWEEN_SCENARIOS'] == '1'
-      log 'New scenario - reinstalling apps'
-    else
-      log 'First scenario in feature - reinstalling apps'
-    end
+    log "This is just a test log"
     FeatureMemory.feature = feature
     FeatureMemory.invocation = 1
   else
