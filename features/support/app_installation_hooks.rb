687ff01acbd806996e600c2b4bd047192add671f
Will Pride <wpride@dimagi.com>
2018-04-30 10:06:24 -0400

    Remove reset_between_scenarios check

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -8,7 +8,7 @@ Before do |scenario|
   scenario = scenario.scenario_outline if scenario.respond_to?(:scenario_outline)
 
   feature = scenario.feature
-  if FeatureMemory.feature != feature || ENV['RESET_BETWEEN_SCENARIOS'] == '1'
+  if FeatureMemory.feature != feature
     if ENV['RESET_BETWEEN_SCENARIOS'] == '1'
       log 'New scenario - reinstalling apps'
     else
