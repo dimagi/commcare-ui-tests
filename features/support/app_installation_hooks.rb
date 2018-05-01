c213dd69ebfe8158bd929eae2495e83dd0ec2c2b
Will Pride <wpride@dimagi.com>
2018-05-01 14:49:50 -0400

    test puts

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -1,6 +1,8 @@
 require 'calabash-android/management/app_installation'
 
 AfterConfiguration do |config|
+  puts "Config #{config}"
+  ENV['RESET_BETWEEN_SCENARIOS'] =='0'
   FeatureMemory.feature = nil
 end
 
@@ -9,7 +11,7 @@ Before do |scenario|
 
   feature = scenario.feature
   if FeatureMemory.feature != feature
-    log "This is just a test log"
+    puts("This is just a test log")
     FeatureMemory.feature = feature
     FeatureMemory.invocation = 1
   else
