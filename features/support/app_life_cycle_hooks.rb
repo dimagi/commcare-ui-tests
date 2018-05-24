3d4360c1dc572bfc5eb13c453ec00248906dd45a
Will Pride <wpride@dimagi.com>
2018-05-24 09:44:08 -0400

    revert

--- features/support/app_life_cycle_hooks.rb
+++ features/support/app_life_cycle_hooks.rb
@@ -4,3 +4,10 @@ require 'calabash-android/operations'
 Before do |scenario|
   start_test_server_in_background
 end
+
+After do |scenario|
+  if scenario.failed?
+    screenshot_embed
+  end
+  shutdown_test_server
+end
