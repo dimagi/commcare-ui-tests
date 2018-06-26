0f402a1aa057b1da20eebdbc74442ffeed9a4782
Will Pride <wpride@dimagi.com>
2018-06-26 18:30:27 -0400

    remove cczs

--- features/step_definitions/installation.rb
+++ features/step_definitions/installation.rb
@@ -17,7 +17,7 @@ Then (/^I install the ccz app at "([^\"]*)"$/) do |path|
   press_menu_button()
   tap_when_element_exists("* {text CONTAINS[c] 'Offline install'}")
   #push("features/resource_files/cczs/%s" % path, "/sdcard/%s" % path)
-  step("I enter \"storage/emulated/0/cczs/%s\" into input field number 1" % path)
+  step("I enter \"storage/emulated/0/%s\" into input field number 1" % path)
   hide_soft_keyboard()
 
   # get around bug where the install button is disabled after entering text
@@ -37,7 +37,7 @@ Then (/^I do an offline update to the ccz app at "([^\"]*)"$/) do |path|
   press_menu_button()
   tap_when_element_exists("* {text CONTAINS[c] 'Offline Update'}")
   #push("features/resource_files/ccz_apps/%s" % path, "/sdcard/%s" % path)
-  step("I enter \"storage/emulated/0/cczs/%s\" into input field number 1" % path)
+  step("I enter \"storage/emulated/0/%s\" into input field number 1" % path)
   hide_soft_keyboard()
 
   # get around bug where the install button is disabled after entering text
