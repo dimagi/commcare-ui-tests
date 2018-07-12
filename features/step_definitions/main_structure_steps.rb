9551bd2042e4a2e38652c7ebc26485d95a83db00
Will Pride <wpride@dimagi.com>
2018-07-12 08:37:21 -0400

    Try commenting out sleeps

--- features/step_definitions/main_structure_steps.rb
+++ features/step_definitions/main_structure_steps.rb
@@ -116,12 +116,12 @@ end
 
 Then (/^I rotate to portrait$/) do
   perform_action('set_activity_orientation', 'portrait')
-  step("I wait for 5 seconds")
+  #step("I wait for 5 seconds")
 end
 
 Then (/^I rotate to landscape/) do
   perform_action('set_activity_orientation', 'landscape')
-  step("I wait for 5 seconds")
+  #step("I wait for 5 seconds")
 end
 
 Then (/^I update the app$/) do
